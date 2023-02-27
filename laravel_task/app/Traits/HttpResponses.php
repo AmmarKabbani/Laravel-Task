4d994130f8af5c7fd03e2fe877e015912beeedbc
AmmarKabbani <3mmar.kabbani@gmail.com>
2023-02-27 13:52:18 +0300

    new Controller and trait
    AuthController for login and register API .
    HttpResponses trait for two methods ( success , error )

---
+++ laravel_task/app/Traits/HttpResponses.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Traits;
+
+trait HttpResponses {
+
+
+    /**
+     * use it when the response to the request is a success 
+     * @param $data  
+     * @param string $message
+     * @param int $code
+     */
+    protected function success($data, string $message = null, int $code = 200)
+    {
+        return response()->json([
+            'status' => 'Request was successful.',
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+
+
+    /**
+     * use it when the response to the request is a fail
+     * @param $data  
+     * @param string $message
+     * @param int $code
+     */
+
+    protected function error($data, string $message = null, int $code)
+    {
+        return response()->json([
+            'status' => 'An error has occurred...',
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+}
