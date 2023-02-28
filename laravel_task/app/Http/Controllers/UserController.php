b1651d136bea8ffdec9afd9aac647d5dd84eb56d
AmmarKabbani <3mmar.kabbani@gmail.com>
2023-02-28 09:22:08 +0300

    new Controller
    UserController .

---
+++ laravel_task/app/Http/Controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Traits\HttpResponses;
+
+class UserController extends Controller
+{
+    use HttpResponses;
+    
+}
